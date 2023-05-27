21df4ee75082506eb0149e27a5e1d377f3d3038e
Shawn Yu <shawnyudesign@gmail.com>
2023-05-27 12:56:10 -0400

    houses are apprearing

--- script.js
+++ script.js
@@ -1,132 +1,100 @@
+function randomColor(){
 
-
-
-
-
-
-setInterval(() => {
-    var randNews = Math.floor(Math.random()*capturedResponse.articles.length)
-    let updown = Math.floor(Math.random()*4+1)
-    var randheight = Math.floor(Math.random()*200-100)
     var color;
         var letters = '0123456789ABCDEF';
         var color = '#';
         for (var j = 0; j < 6; j++) {
         color += letters[Math.floor(Math.random() * 16)];}
+    return color
+
+}
+
+function createHouse1(color){
+console.log("createhourse");
+var lot = $("<div>");
+lot.addClass("lot");
+var house1 = $("<div>");
+house1.addClass("house1")
+var randWidth = Math.floor(Math.random()*60+20)
+var randHeight = Math.floor(Math.random()*60+20)
+
+var roof1 = $("<div>");
+roof1.addClass("roof1");
+var chosenColor = randomColor();
+var windowColor = "black";
+var windowBorderColor = randomColor();
+var roofColor = randomColor();
+house1.css("background-color", chosenColor);
+house1.css("width",randWidth + "%")
+house1.css("height", randHeight + "%")
+house1.css("top", (100-randHeight) + "%")
+
+roof1.css("background-color", chosenColor);
+roof1.css("border-color", roofColor);
+roof1.css("width",Math.sqrt(randWidth^2*2) + "%")
+house1.append(roof1);
+var randWindowNumber = Math.floor(Math.random()*6+2)
+
+
+for(i=0; i<randWindowNumber; i++){
+    var window = $("<div>");
+    window.addClass("window");
+    window.css("background-color", windowColor);
+    window.css("border-color",windowBorderColor)
+    var windowBar = $("<div>");
     
-    var stripe1 = $("<div>")
-    stripe1.addClass("stripe1");
-    stripe1.css("background-color",color)
-    stripe1.css("top",randheight+"%")
-    stripe1.attr("number",randNews)
-    stripe1.html(capturedResponse.articles[randNews].title)
-
-  
-    switch(updown){
-        case 1 :$('.left').append(stripe1)
-        break;
-        case 2 :$('.right').append(stripe1)
-        // break;
-        // case 3 :$('.left2').append(stripe1)
-        // break;
-        // case 4 :$('.right2').append(stripe1)
-        
-    }
-  }, 500);
-
-
-  setInterval(() => {
-    var randNews = Math.floor(Math.random()*capturedResponse.articles.length)
-
-    let updown = Math.floor(Math.random()*4+1)
-    var randheight = Math.floor(Math.random()*200-100)
-     color;
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var j = 0; j < 6; j++) {
-        color += letters[Math.floor(Math.random() * 16)];}
+    windowBar.addClass("windowBarHori")
+    windowBar.css("background-color",windowBorderColor)
+    window.append(windowBar)
+    if(randWidth>60){
+    var windowBarVert = $("<div>");
     
-    var stripe1 = $("<div>")
-    stripe1.addClass("stripe2");
-    stripe1.css("background-color",color)
-    stripe1.css("left",randheight+"%")
-    stripe1.attr("number",randNews)
-    stripe1.html(capturedResponse.articles[randNews].title)
+    windowBarVert.addClass("windowBarVert")
+    windowBarVert.css("background-color", windowBorderColor)
+    window.append(windowBarVert)
 
-  
-    switch(updown){
-        case 1 :$('.top').append(stripe1)
-        break;
-        case 2 :$('.bottom').append(stripe1)
-        break;
-        case 3 :$('.bottom2').append(stripe1)
-        // break;
-        // case 4 :$('.bottom').append(stripe1)
+    }
+    var top
+    var left
+    if(i<randWindowNumber/2){
+        top=60;
+        left = 34.5*i + 20*i
+
+
+    }
+    else{
+        top=20
+       left = 34.5*(i-randWindowNumber/2) + 20*(i-randWindowNumber/2)
+
+
+    }
+    window.css("top", top + "%")
+    window.css("left", left + "%");
+    house1.append(window);
+
+
+}
+
+lot.append(house1);
+$("body").append(lot)
+}
+
+$("body").keypress(event=>{
+    event.stopPropagation();
+    event.preventDefault();
+    var pianoKey=event.key
+    console.log(pianoKey)
+    if(pianoKey=='w'){
         
+        createHouse1();
+
     }
-  }, 500);
-
-  setInterval(() => {
-    var randNews = Math.floor(Math.random()*capturedResponse.articles.length)
-
-    let updown = Math.floor(Math.random()*4+1)
-    var randheight = Math.floor(Math.random()*250-150)
-     color;
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var j = 0; j < 6; j++) {
-        color += letters[Math.floor(Math.random() * 16)];}
-    
-    var stripe3 = $("<img>")
-    stripe3.addClass("stripe3");
-    stripe3.css("background-color",color)
-    stripe3.css("top",randheight+"%")
-    stripe3.attr("number",randNews)
-    stripe3.attr("src", capturedResponse.articles[randNews].image)
-
   
-    switch(updown){
-        case 1 :$('.right').append(stripe3)
-        break;
-        case 2 :$('.left').append(stripe3)
-        // break;
-        // case 3 :$('.left2').append(stripe3)
-        // break;
-        // case 4 :$('.right2').append(stripe3)
-        
-    }
-  }, 800);
 
-  setInterval(() => {
-     var stripes1= $(".stripe1");
-     for(i=0; i<stripes1.length;i++){
-         if($(stripes1[i]).css("visibility")=="hidden"){
-             $(stripes1[i]).remove();
-         }
-     }
-      
-  }, 200);
-
-  setInterval(() => {
-    var stripes2= $(".stripe2");
-    for(i=0; i<stripes2.length;i++){
-        if($(stripes2[i]).css("visibility")=="hidden"){
-            $(stripes2[i]).remove();
-        }
-    }
-     
- }, 200);
- setInterval(() => {
-    var stripes3= $(".stripe3");
-    for(i=0; i<stripes3.length;i++){
-        if($(stripes3[i]).css("visibility")=="hidden"){
-            $(stripes3[i]).remove();
-        }
-    }
-     
- }, 200);
+})
 
 
+// 
 
     // var color;
     // var letters = '0123456789ABCDEF';
@@ -136,20 +104,3 @@ setInterval(() => {
  
 
 
-$(document).click(event=>{
-    console.log($(event.target))
-})
-
-$(document).on("click",".screen",event=>{
-    $(".screen").removeClass("open")
-    $(".screen").addClass("invisibleP");
-    $(".newsTitle").removeClass("newsTitleOpen")
-    $(".newsTitle").addClass("invisibleP");
-    $(".newsText").addClass("invisibleP");
-    $(".newsText").removeClass("newsTextOpen");
-    $(".newsImage").addClass("invisibleP");
-    $(".newsImage").removeClass("newsImageOpen");
-    $(".newsLink").addClass("invisibleP");
-    $(".newsLink").removeClass("newsLinkOpen");
-
-})
