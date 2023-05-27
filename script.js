f5fe88ff3d3b7a15df332ac2cac75b8c6c6da289
Shawn Yu <shawnyudesign@gmail.com>
2023-05-27 15:37:40 -0400

    sdsdsd

--- script.js
+++ script.js
@@ -9,14 +9,32 @@ function randomColor(){
 
 }
 
-function createHouse1(color){
-console.log("createhourse");
+const DarkColors = [
+
+
+    
+]
+function randomDarkColor(){
+
+    var color;
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var j = 0; j < 6; j++) {
+        color += letters[Math.floor(Math.random() * 16)];}
+    return color
+
+}
+
+
+
+function createHouse2(color){
+console.log("createhourse2");
 var lot = $("<div>");
 lot.addClass("lot");
 var house1 = $("<div>");
 house1.addClass("house1")
-var randWidth = Math.floor(Math.random()*60+20)
-var randHeight = Math.floor(Math.random()*60+20)
+var randWidth = Math.floor(Math.random()*30+50)
+var randHeight = Math.floor(Math.random()*100+80)
 
 var roof1 = $("<div>");
 roof1.addClass("roof1");
@@ -81,6 +99,98 @@ lot.append(house1);
 $(".left").append(lot)
 }
 
+function deleteHouse(){
+var Lots = $(".lot")
+if(Lots&&Lots.length>10){
+ Lots[0].remove();
+    
+}
+
+}
+
+
+
+function createHouse1(color){
+console.log("createhourse");
+var lot = $("<div>");
+lot.addClass("lot");
+var house1 = $("<div>");
+house1.addClass("house1")
+var randWidth = Math.floor(Math.random()*30+50)
+var randHeight = Math.floor(Math.random()*100+80)
+
+var roof1 = $("<div>");
+roof1.addClass("roof1");
+var chosenColor = randomColor();
+var windowColor = "black";
+var windowBorderColor = randomColor();
+var roofColor = randomColor();
+house1.css("background-color", chosenColor);
+house1.css("width",randWidth + "%")
+house1.css("height", randHeight + "%")
+house1.css("top", (100-randHeight) + "%")
+
+roof1.css("background-color", chosenColor);
+roof1.css("border-color", roofColor);
+roof1.css("width",Math.sqrt(5000) + "%")
+roof1.css("padding-bottom",Math.sqrt(5000) + "%")
+
+house1.append(roof1);
+var randWindowNumber = Math.floor(Math.random()*6+2)
+
+
+for(i=0; i<randWindowNumber; i++){
+    var window = $("<div>");
+    window.addClass("window");
+    window.css("background-color", windowColor);
+    window.css("border-color",windowBorderColor)
+    var windowBar = $("<div>");
+    
+    windowBar.addClass("windowBarHori")
+    windowBar.css("background-color",windowBorderColor)
+    window.append(windowBar)
+    if(randWidth>60){
+    var windowBarVert = $("<div>");
+    
+    windowBarVert.addClass("windowBarVert")
+    windowBarVert.css("background-color", windowBorderColor)
+    window.append(windowBarVert)
+
+    }
+    var top
+    var left
+    if(i<randWindowNumber/2){
+        top=60;
+        left = 5*(i+1) + 20*(i)
+
+
+    }
+    else{
+        top=20
+       left = 5*(i-Math.floor(randWindowNumber/2)+1) + 20*(i-randWindowNumber/2)
+
+
+    }
+    window.css("top", top + "%")
+    window.css("left", left + "%");
+    house1.append(window);
+
+
+}
+
+lot.append(house1);
+$(".left").append(lot)
+}
+
+function deleteHouse(){
+var Lots = $(".lot")
+if(Lots&&Lots.length>10){
+ Lots[0].remove();
+    
+}
+
+}
+
 $("body").keypress(event=>{
     event.stopPropagation();
     event.preventDefault();
@@ -89,6 +199,8 @@ $("body").keypress(event=>{
     if(pianoKey=='w'){
         
         createHouse1();
+        deleteHouse();
+
 
     }
   
